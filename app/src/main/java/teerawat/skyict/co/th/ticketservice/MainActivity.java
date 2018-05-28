0e138c619f116c8de41c46ecc771c85fad6fe654
teerawat <teerawat@skyict.co.th>
2018-05-28 11:23:38 +0700

    Create MainFragment Success

--- app/src/main/java/teerawat/skyict/co/th/ticketservice/MainActivity.java
+++ app/src/main/java/teerawat/skyict/co/th/ticketservice/MainActivity.java
@@ -3,11 +3,25 @@ package teerawat.skyict.co.th.ticketservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import teerawat.skyict.co.th.ticketservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add fragment
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+    }//Main Method
+
+} //MainActivity
