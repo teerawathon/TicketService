e78aca7cf21eb567e8c608eba7cc8c8a242b4af0
teerawat <teerawat@skyict.co.th>
2018-05-29 14:02:09 +0700

    Help me

--- app/src/main/java/teerawat/skyict/co/th/ticketservice/ServiceActivity.java
+++ app/src/main/java/teerawat/skyict/co/th/ticketservice/ServiceActivity.java
@@ -12,6 +12,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import teerawat.skyict.co.th.ticketservice.utility.ListViewAdapter;
+import teerawat.skyict.co.th.ticketservice.utility.MyConstance;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -32,8 +36,23 @@ public class ServiceActivity extends AppCompatActivity {
 //        Create Toolbar
         createToolbar();
 
+//        Create ListView
+        createListView();
 
     } //Main method
+
+    private void createListView() {
+
+        ListView listView = findViewById(R.id.myListview);
+        MyConstance myConstance = new MyConstance();
+
+        String[] titlestrings = myConstance.getTitleListStrings();
+        int[] IconInts = myConstance.getIconInts();
+        ListViewAdapter listViewAdapter = new ListViewAdapter(ServiceActivity.this,
+                titlestrings, IconInts);
+       // listView.setAdapter(listViewAdapter);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/teerawat/skyict/co/th/ticketservice/utility/MyConstance.java
+++ app/src/main/java/teerawat/skyict/co/th/ticketservice/utility/MyConstance.java
@@ -1,8 +1,23 @@
 package teerawat.skyict.co.th.ticketservice.utility;
 
+import teerawat.skyict.co.th.ticketservice.R;
+
 public class MyConstance {
 
     private String[] titleListStrings = new String[]{"Ticket", "New Ticket", "Report", "About"};
 
+    private int[] iconInts = new int[]{
+            R.drawable.ic_action_ticket,
+            R.drawable.ic_action_new_ticket,
+            R.drawable.ic_action_report,
+            R.drawable.ic_action_about
+    };
 
+    public String[] getTitleListStrings() {
+        return titleListStrings;
+    }
+
+    public int[] getIconInts() {
+        return iconInts;
+    }
 }
