1145ddcf703917e080d5edf9c3b0bc076551bc81
teerawat <teerawat@skyict.co.th>
2018-05-30 13:52:32 +0700

    New Ticket OK

--- app/src/main/java/teerawat/skyict/co/th/ticketservice/fragment/NewItemFragment.java
+++ app/src/main/java/teerawat/skyict/co/th/ticketservice/fragment/NewItemFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import teerawat.skyict.co.th.ticketservice.R;
 import teerawat.skyict.co.th.ticketservice.utility.MyConstance;
+import teerawat.skyict.co.th.ticketservice.utility.ReadAllData;
+import teerawat.skyict.co.th.ticketservice.utility.ShowListTicketAdapter;
 
 public class NewItemFragment extends Fragment {
 
@@ -30,6 +36,46 @@ public class NewItemFragment extends Fragment {
 
             MyConstance myConstance = new MyConstance();
             String urlJSON = myConstance.getUrlNewItemString();
+
+            ReadAllData readAllData = new ReadAllData(getContext());
+            readAllData.execute(urlJSON);
+            String jsonString = readAllData.get();
+            Log.d("30MayV1", "JSON ==>" + jsonString);
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            String[] docNoStrings = new String[jsonArray.length()];
+            String[] serialStrings = new String[jsonArray.length()];
+            String[] detailStrings = new String[jsonArray.length()];
+            String[] severityStrings = new String[jsonArray.length()];
+            String[] assignStrings = new String[jsonArray.length()];
+            String[] statusStrings = new String[jsonArray.length()];
+            String[] dueDateStrings = new String[jsonArray.length()];
+
+            for (int i = 0; i < jsonArray.length(); i += 1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                docNoStrings[i] = jsonObject.getString("DocNo");
+                serialStrings[i] = jsonObject.getString("SerialNumber");
+                detailStrings[i] = jsonObject.getString("Detail");
+                severityStrings[i] = jsonObject.getString("Severity");
+                assignStrings[i] = jsonObject.getString("Assignee");
+                statusStrings[i] = jsonObject.getString("Status");
+                dueDateStrings[i] = jsonObject.getString("DueDate");
+
+            }   //for
+
+            ShowListTicketAdapter showListTicketAdapter = new ShowListTicketAdapter(getActivity(),
+                    docNoStrings,
+                    serialStrings,
+                    detailStrings,
+                    severityStrings,
+                    assignStrings,
+                    statusStrings,
+                    dueDateStrings);
+            listView.setAdapter(showListTicketAdapter);
+
 
         } catch (Exception e) {
             e.printStackTrace();
